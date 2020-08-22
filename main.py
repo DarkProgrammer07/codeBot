82754aa2336088d81ac01cf0ddec84a682508e77
DarkProgrammer07 <69521295+DarkProgrammer07@users.noreply.github.com>
2020-08-22 16:56:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ async def on_ready():
     print(f'Bot is alive in {len(client.guilds)} guild(s)')
 
 if __name__ == '__main__':
-    with open('.token') as tokenFile:
-        token = tokenFile.read()
-
     for _ in cogList:
         client.load_extension('cogs.' + _)
 
