d8ef5013aa5eb68f1582f72b1a113f9cedfcdb65
DarkProgrammer07 <69521295+DarkProgrammer07@users.noreply.github.com>
2020-08-22 17:08:11 +0530

    Update messageListener.py

--- cogs/messageListener.py
+++ cogs/messageListener.py
@@ -9,7 +9,7 @@ class MessageListener(commands.Cog):
         if message.channel.id == 739538674349506642:
             if message.content.lower() == '&verify':
                 await message.author.add_roles(message.guild.get_role(739346618821771286), reason='verified')
-                await message.author.guild.get_channel(739341264255975466).send(f'{message.author.mention}, **whalecum to `code break;` 🐳💦**')
+                await message.author.guild.get_channel(739341264255975466).send(f'{message.author.mention}, **whalecum to `std::code` 🐳💦**')
                 await message.delete()
             else:
                 await message.delete()
